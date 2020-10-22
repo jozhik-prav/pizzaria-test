93664533b5fe905d320495b8d1a814b502ca4bd7
Александра Грахова <grahova_sasha@mail.ru>
2020-10-22 19:00:37 +0300

    Создала компонент с карточкой товара

---
+++ Pizzeria/ClientApp/src/components/MenuItem.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="card">
+        <div class="card-image">
+            <img src="../assets/1.png" alt="">
+        </div>
+        <div class="card-content">
+            <h5 class="card-title">Гавайская</h5>
+            <p class="card-text">Цыпленок, томатный соус, моцарелла, ананасы</p>
+        </div>
+        <div class="card-footer">
+            <p class="card-price">425 ₽</p>
+            <button class="card-btn">Добавить</button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class MenuItem extends Vue {
+    
+}
+</script>
+
+<style lang="scss">
+    .card {
+        width: 25%;
+        padding: 0 15px;
+        margin-bottom: 10px;
+    }
+
+    .card-image {
+        img {
+            display: block;
+            margin: 0 auto;
+        }
+    }
+
+    .card-content {
+        margin-bottom: 10px;
+    }
+
+    .card-title {
+        color: #fdbc2c;
+        font-size: 24px;
+        padding: 10px 0;
+    }
+
+    .card-text {
+        font-size: 17px;
+    }
+
+    .card-footer {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .card-price {
+         color: #D94F2B;
+         font-size: 24px;
+     }
+
+    .card-btn {
+        background-color: #fdbc2c;
+        padding: 4px 20px;
+        border: 1px solid #fdbc2c;
+        color: #000;
+        font-size: 14px;
+        font-weight: 700;
+    }
+</style>
--- Pizzeria/ClientApp/src/views/Menu.vue
+++ Pizzeria/ClientApp/src/views/Menu.vue
@@ -1,17 +1,36 @@
 <template>
   <div>
       <FilterMenu></FilterMenu>
+      <div class="menu-container">
+          <MenuItem></MenuItem>
+          <MenuItem></MenuItem>
+          <MenuItem></MenuItem>
+          <MenuItem></MenuItem>
+          <MenuItem></MenuItem>
+          <MenuItem></MenuItem>
+          <MenuItem></MenuItem>
+      </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import FilterMenu from "@/components/FilterMenu.vue"; // @ is an alias to /src
+import FilterMenu from "@/components/FilterMenu.vue";
+import MenuItem from "@/components/MenuItem.vue";
 
 @Component({
   components: {
-   FilterMenu
+   FilterMenu,
+   MenuItem
   }
 })
 export default class Menu extends Vue {}
 </script>
+
+<style lang="scss">
+    .menu-container {
+        display: flex;
+        flex-wrap: wrap;
+        padding-top: 20px;
+    }
+</style>
