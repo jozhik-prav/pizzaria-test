c1ea2efc6417aea52df5ba0c778f137c29d72899
Александра Грахова <grahova_sasha@mail.ru>
2020-10-23 14:41:47 +0300

    Создала компонент с корзиной

---
+++ Pizzeria/ClientApp/src/components/BasketItem.vue
@@ -0,0 +1,89 @@
+<template>
+    <article class="item">
+        <img class="item-img" src="../assets/1.png" alt="">
+        <main class="item-content">
+            <h5>Гавайская</h5>
+            <p>Цыпленок, томатный соус, моцарелла, ананасы</p>
+        </main>
+        <div class="item-control">
+            <button class="control-btn"> - </button>
+            <span> 2 </span>
+            <button class="control-btn"> + </button>
+        </div>
+        <div class="item-price">890 ₽</div>
+        <div class="item-delete">Удалить</div>
+    </article>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class BasketItem extends Vue {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    .item {
+        display: flex;
+        align-items: center;
+        box-shadow: 0px 2px 0px 0px #010202;
+        border-bottom: 1px solid #383838;
+    }
+
+    .item-img {
+        width: 100px;
+        flex: 0 0 auto;
+        margin-right: 12px;
+    }
+    
+    .item-content {
+        display: flex;
+        flex: 1 1 auto;
+        flex-flow: column;
+        justify-content: center;
+        h5 {
+            color: #fdbc2c;
+            font-size: 24px;
+        }
+    }
+
+    .item-control {
+        flex: 0 0 auto;
+        display: flex;
+        align-items: center;
+        border: 1px solid #fdbc2c;
+        border-radius: 5px;
+        .control-btn {
+            background: transparent;
+            color: #fff;
+            padding: 0 20px;
+            border: none;
+            outline: none;
+            font-size: 24px;
+            cursor: pointer;
+
+            &:hover {
+                color: #D94F2B;
+            }
+        }
+    }
+
+    .item-price {
+        color: #D94F2B;
+        font-size: 24px;
+        flex: 0 0 auto;
+        display: flex;
+        flex-flow: column;
+        justify-content: center;
+        padding: 0 10px;
+        margin: 0 12px;
+    }
+
+    .item-delete {
+        display: flex;
+        flex-flow: column;
+        justify-content: center;
+    }
+</style>
---
+++ Pizzeria/ClientApp/src/views/Basket.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="basket-container">
+        <h1 class="container-title">Корзина</h1>
+        <BasketItem></BasketItem>
+        <BasketItem></BasketItem>
+        <div class="basket-total">
+            <div class="total-label">Сумма заказа:</div>
+            <div class="total-value">890 ₽</div>
+        </div>
+        <div class="basket-footer">
+            <button class="btn btn-outline-primary">Вернуться в меню</button>
+            <button class="btn btn-primary">Оформить заказ</button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import BasketItem from "@/components/BasketItem.vue";
+
+@Component({
+  components: {
+   BasketItem
+  }
+})
+export default class Basket extends Vue {}
+</script>
+
+<style lang="scss">
+    .container-title {
+        border-bottom: 4px solid #D94F2B;
+        margin-bottom: 20px;
+        padding-bottom: 10px;
+    }
+
+    .basket-total {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        font-size: 1.5rem;
+        font-weight: 700;
+        margin: 20px 0;
+    }
+
+    .total-value {
+        color: #D94F2B;
+        padding-left: 10px;
+        font-size: 2rem;
+    }
+
+    .basket-footer {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .btn {
+        margin-bottom: 14px;
+        padding: 10px 25px;
+        font-size: 14px;
+        font-weight: 700;
+        border-width: 2px;
+        border-style: solid;
+        transition: all 0.3s;
+        text-transform: uppercase;
+        background: transparent;
+        cursor: pointer;
+    }
+
+    .btn-primary {
+        background-color: #fdbc2c;
+        border-color: #fdbc2c;
+        &:hover {
+            background: #D94F2B;
+            color: #fff;
+        }
+    }
+
+    .btn-outline-primary {
+        border-color: #fdbc2c;
+        color: #fff;
+        &:hover {
+            background: #D94F2B;
+        }
+    }
+</style>
